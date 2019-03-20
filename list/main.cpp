43330b7aa9b605bbcf30e3cb4af53177bade5e17
DethleSS <rik546@mail.ru>
2019-03-20 16:26:16 +0300

    List

---
+++ list/main.cpp
@@ -0,0 +1,22 @@
+#include <QCoreApplication>
+#include "list.h"
+#include <iostream>
+#include <algorithm>
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    stu::list<int> b{1,-2,3};
+    auto r = std::find(b.begin(), b.end(),-2);
+    b.insert_after(r,4);
+    for(auto i = b.begin(); i != b.end(); ++i)
+    {
+        std::cout << *i <<" ";
+
+    }
+//    std::cout << b.find_last()->m_value;
+//    std::cout << (b.begin() == b.end());
+//    std::cout << b.head->m_value <<" ";
+//    std::cout << b.head->next->m_value;
+    return a.exec();
+}
